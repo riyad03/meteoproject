ecbf4aef86de8d1f8a3e90542b7669656abd8de9
riyad03 <riyad.rachidi5@gmail.com>
2025-07-17 22:47:04 +0100

    minor add: ability to import from front

---
+++ springcloudgateway/src/main/java/com/example/springcloudgateway/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.example.springcloudgateway.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("*")); // or List.of("http://localhost:3000") for specific origins
+        corsConfig.setMaxAge(8000L);
+        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(false); // set true if cookies/auth headers are used
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
